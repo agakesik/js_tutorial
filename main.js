380d6deca2d545ce725c3abc54b797b528f63845
Aga <aga.k396@gmail.com>
2020-02-27 15:28:25 +0100

    Create prompt for user to put their string for testing

--- main.js
+++ main.js
@@ -1,2 +1,9 @@
 let Phrase = require("aga-palindrome");
-alert(new Phrase("Madam, I'm Adam").palindrome());
+let string = prompt("Please enter a string for palindrome:")
+let phrase = new Phrase(string);
+
+if (phrase.palindrome()) {
+  alert(`"${phrase.content}" is a palindrome`);
+} else {
+  alert(`${phrase.content} is not a palindrome.`);
+}
