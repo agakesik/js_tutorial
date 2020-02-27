83ae3cd1a72fecce8ea419bdb42002fdd160ce42
Aga <aga.k396@gmail.com>
2020-02-27 15:48:25 +0100

    Set up to create button asking about palindrome

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
 let Phrase = require("aga-palindrome");
-let string = prompt("Please enter a string for palindrome:")
-let phrase = new Phrase(string);
 
-if (phrase.palindrome()) {
-  alert(`"${phrase.content}" is a palindrome`);
-} else {
-  alert(`${phrase.content} is not a palindrome.`);
-}
+// function palindromeTester() {
+  let string = prompt("Please enter a string for palindrome:")
+  let phrase = new Phrase(string);
+
+  if (phrase.palindrome()) {
+    alert(`"${phrase.content}" is a palindrome`);
+  } else {
+    alert(`${phrase.content} is not a palindrome.`);
+  }
+// }
