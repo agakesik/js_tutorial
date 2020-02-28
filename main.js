64828abbcf358102bdc1ed8f84b1e5e34a2eb940
Aga <aga.k396@gmail.com>
2020-02-28 11:01:05 +0100

    add option make palindrome test on demand using button

--- main.js
+++ main.js
@@ -1,12 +1,16 @@
 let Phrase = require("aga-palindrome");
 
-// function palindromeTester() {
-  let string = prompt("Please enter a string for palindrome:")
+function palindromeTester() {
+  let string = prompt(" enter a string for palindrome:")
   let phrase = new Phrase(string);
 
   if (phrase.palindrome()) {
-    alert(`"${phrase.content}" is a palindrome`);
+    alert(`"${phrase.content}" is a palindrome :D`);
   } else {
-    alert(`${phrase.content} is not a palindrome.`);
+    alert(`${phrase.content} is not a palindrome. :C`);
   }
-// }
+}
+document.addEventListener("DOMContentLoaded", function(){
+  let button = document.querySelector("#palindromeTester");
+  button.addEventListener("click", palindromeTester);
+});
